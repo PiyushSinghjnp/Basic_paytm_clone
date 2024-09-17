d216f33414a62f9ab39f29a50b19187722d8c3a7
piyushsinghjaunpur <ps450491@gmail.com>
2024-09-17 21:31:21 +0530

    changes in the sign in page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -78,7 +78,7 @@ export const  Signin = ()=>{
           <BottomWarning
             label={"Don't have an account?"}
             buttonText={"Sign up"}
-            to={"/signup"}
+            to={"/"}
           />
         </div>
       </div>
